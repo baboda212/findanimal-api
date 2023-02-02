28b7f600355f61693ed0876dfa4288b18eac75a5
baboda212 <baboda212@naver.com>
2023-02-02 19:37:07 +0900

    svg 애니메이션 효과적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,12 @@ import style from './style.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div class={style.wrap}>
         <Nav/>
+        <div class={style.home_text}>
+          <h1>당신의 소중한 결정이 이 동물친구들에게는 삶을 지속할 수 있는 힘이 됩니다.</h1>
+          <a href='/about'>동물친구들 만나러 가기</a>
+        </div>
         <div class={style.container}>
           <div class={style.bk}>
             <div class={style.mid}>
