861c73420751d0c72dafcfdf5ab00a2422d84d3f
baboda212 <baboda212@naver.com>
2023-02-08 17:42:43 +0900

    검색

--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useRef} from 'react';
 //라우터에 연결할 페이지 불러오기
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 
 function App() {
-  const API_KEY = `%2BefalOjB2%2F4P8zlVJ%2BVlLxjqN1PS6NrVpqtyI3G%2F9WERm2OZRIB57ocCGqM81E5hIUU6%2F2LYYVyEgMxVauj6Sw%3D%3D`;
-  const API_URL = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20230101&endde=20231231&pageNo=1&numOfRows=100&serviceKey=${API_KEY}&_type=json`;
-
   const [animals, setAnimals] = useState([]);
+  const [totalCount, setTotalCount] = useState(0); // 총 글수
+  const [pageNo, setPageNo] = useState(1);  // 페이지번호
+  const [maxPages, setMaxPages] = useState(0); // 마지막 페이지
+  const [rows, setRows] = useState(10); // 화면당 글수
+  const [search, setSearch] = useState(''); //검색어
+  const inputRef = useRef();//검색창요소의 위치 참조
+  
+ 
 
+    const getAnimalData =  () =>{
+      const API_KEY = `%2BefalOjB2%2F4P8zlVJ%2BVlLxjqN1PS6NrVpqtyI3G%2F9WERm2OZRIB57ocCGqM81E5hIUU6%2F2LYYVyEgMxVauj6Sw%3D%3D`;
+      const API_URL = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20230101&endde=20231231&pageNo=${pageNo}&numOfRows=${rows}&serviceKey=${API_KEY}&_type=json${search}`;
 
-  const getAnimalData = () =>{
     fetch(API_URL)
     .then(res => res.json())
     .then(data =>{
       console.log(data.response.body.items.item);
       const items = data.response.body.items.item;
+      // 데이터를 state에 저장
       setAnimals(items);
+
+      //전체글
+      setTotalCount(data.response.body.totalCount);
+      // 마지막 페이지(총게시물 / 행수)
+      setMaxPages(Math.ceil(totalCount / rows));
+      //console.log(Math.ceil(totalCount / rows))
+      
     })
+    .catch(() =>{
+      console.log('데이터 요청 에러')
+      setAnimals([])
+    })
+  };
+
+  // 이전 페이지
+  const nextPage = () => {
+    if(pageNo < maxPages) setPageNo(pageNo + 1)
   }
 
+  // 다음 페이지
+  const prevPage = () => {
+    if(pageNo > 1) setPageNo(pageNo - 1);
+  }
+
+  //페이지번호 바뀌면API요청
   useEffect(() => {
-    getAnimalData()
-  }, []);
+    getAnimalData();
+
+  }, [ pageNo, totalCount, search ])
+
+  //console.log('animals = ', animals)
+  //console.log('totalCount = ', totalCount)
+  //console.log('maxPages = ', maxPages);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About animals = {animals}/>} />
+        <Route path='/about' element={<About animals = {animals} pageNo = {pageNo} maxPages ={maxPages} nextPage = {nextPage} prevPage = {prevPage} totalCount = {totalCount} inputRef = {inputRef} setSearch = {setSearch} setPageNo={setPageNo} />} />
         <Route path='/projects' element={<Projects/>} />
       </Routes>
     </BrowserRouter>
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,17 +3,44 @@ import React from 'react';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
-export default function About({animals}) {
+export default function About({animals, pageNo, maxPages, prevPage, nextPage, totalCount, inputRef, setSearch, setPageNo}) {
   
   return (
     <div className='about'>
         <Nav/>
         <div className='about-container'>
           <h1><span>유</span><span>기</span><span>동</span><span>물</span><span>을</span> <span>지</span><span>켜</span><span>줘</span></h1>
-          {/* <form action="">
-            <input type="text" value="search" placeholder="품종을 검색하세요"/>
-            <button type='submit'>검색</button>
-          </form> */}
+              
+          <form onSubmit={(e) =>{
+              e.preventDefault();
+              //입력창에 입력된 값 참조(ref={inputRef})
+              let val = inputRef.current.value;//입력요소의 현재 값
+              console.log("입력요소",val);
+              //console.log("지역",animals[0].orgNm)
+              if (val === '') {
+                setSearch(val); //아무입력이 없을때 전체 보여주기
+              } else {
+                setSearch(`&orgNm=${val}`);//입력값이 있으면
+              
+              }
+              setPageNo(1)
+            }}>
+              <input
+              onInput={(e)=>{
+                //console.log(e.target.value);
+              }}
+              ref={inputRef} type='search' name='s' placeholder='검색어 입력'
+              ></input>
+              <button>검색</button>
+          </form>
+
+          <p>Page: {pageNo} / {maxPages}</p>
+          <p>{totalCount}건 등록</p>
+          <div className='btngroup'>
+            <button className='btnNext' onClick={prevPage}>이전 페이지</button>
+            <button className='btnNext' onClick={nextPage}>다음 페이지</button>
+          </div>
+
           {
             animals.map((animal, key) => {
               return(
