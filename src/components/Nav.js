0027b5575c89f28304a88b53ab744f8c8c4d98d9
baboda212 <baboda212@naver.com>
2023-02-09 14:43:10 +0900

    아이콘 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,16 +8,15 @@ export default function Nav() {
   return (
     <div>
          <nav className='nav'>
-          <div className='logoimg'>
-            <img src={img[0]} alt='logo'/>
-            <img src={img[1]} alt='logo1'/>
-          </div>
             <div className='gnb'>
+              <img src={img[0]} alt='logo'/>
               <Link to='/'>Home</Link>
               <Link to='/about'>유기동물친구 소개</Link>
               <Link to='/projects'>입양하기 전에</Link>
+              <img src={img[1]} alt='logo1'/>
             </div>
         </nav>
+        
     </div>
   )
 }
